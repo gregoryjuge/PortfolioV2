fa0773f9bfaab06bb39b8ab95f48a3aca8cb7312
Gregory Juge <gregoryjuge5@gmail.com>
2022-11-17 19:32:57 +0100

    add team component & fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import React from "react";
+// import "./App.css";
+import Team from "./components/team/team";
+import { useEffect, useState } from "react";
+
+// Parses the JSON returned by a network request
+const parseJSON = (resp) => (resp.json ? resp.json() : resp);
+
+// Checks if a network request came back fine, and throws an error if not
+const checkStatus = (resp) => {
+  if (resp.status >= 200 && resp.status < 300) {
+    return resp;
+  }
+
+  return parseJSON(resp).then((resp) => {
+    throw resp;
+  });
+};
+
+const headers = { "Content-Type": "application/json" };
 
 const App = () => {
+  const [error, setError] = useState(null);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:1337/api/restaurants", { headers, method: "GET" })
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(({ data }) => setRestaurants(data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    // Print errors if any
+    return <div>An error occured: {error.message}</div>;
+  }
+  console.log(restaurants);
   return (
     <div className="App">
-      <ul>HELLO WORLD !</ul>
+      <Team />
+      <ul>
+        {restaurants.map(({ id, attributes }) => (
+          <h2 key={id}>{attributes.name}</h2>
+        ))}
+        {/* {restaurants.map(({ id, attributes }) => (
+          <p key={id}>{attributes.description}</p>
+        ))} */}
+      </ul>
     </div>
   );
 };
