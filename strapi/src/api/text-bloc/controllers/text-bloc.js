11f9f9ceb2396ef97d5964b22df79c266fec43c2
Gregory Juge <gregoryjuge5@gmail.com>
2022-11-22 16:41:43 +0100

    final fixe for big mistake

--- strapi/src/api/text-bloc/controllers/text-bloc.js
+++ strapi/src/api/text-bloc/controllers/text-bloc.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category controller
+ * text-bloc controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::text-bloc.text-bloc');
