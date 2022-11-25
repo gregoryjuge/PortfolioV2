4cf898c50230d84027323fb79c8fb0a1d39e2a43
Gregory Juge <gregoryjuge5@gmail.com>
2022-11-25 09:46:50 +0100

    add cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Hero from "./components/hero/hero";
 import Team from "./components/team/team";
+import Cards from "./components/cards/projects-cards";
 
 const App = () => {
   return (
     <div className="App">
       <Hero />
       <Team />
+      <Cards />
     </div>
   );
 };
