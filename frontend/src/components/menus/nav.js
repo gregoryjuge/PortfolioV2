3daa71a8a2cf3ea66aab701f8cff893990a6eceb
Gregory Juge <gregoryjuge5@gmail.com>
2022-12-20 16:12:44 +0100

    burger menu is dynamic

--- frontend/src/components/menus/nav.js
+++ frontend/src/components/menus/nav.js
@@ -1,26 +1,55 @@
 import React from "react";
 import "./nav.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+const parseJSON = (resp) => (resp.json ? resp.json() : resp);
+
+const checkStatus = (resp) => {
+  if (resp.status >= 200 && resp.status < 300) {
+    return resp;
+  }
+
+  return parseJSON(resp).then((resp) => {
+    throw resp;
+  });
+};
+const headers = { "Content-Type": "application/json" };
 
 function Nav() {
   const [showLinks, setshowLinks] = useState(false);
   const handleShowLinks = () => {
     setshowLinks(!showLinks);
   };
+  const [error, setError] = useState(null);
+  const [contentMenus, setcontentMenus] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:1337/api/menus", { headers, method: "GET" })
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(({ data }) => setcontentMenus(data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    return <div>An error occured: {error.message}</div>;
+  }
   return (
     <nav
       className={` navbar ${showLinks ? "show-nav" : "hide-nav"} md:invisible `}
     >
-      <ul className="navbar__links">
+      <ul className="navbar__links" onClick={handleShowLinks}>
         <li className="navbar__item slideInDown-1">
-          <a href="/" className="navbar__link">
-            Projets
-          </a>
+          {contentMenus.map(({ id, attributes }) => (
+            <a href={attributes.menu_link} key={id} className="navbar__link">
+              {attributes.menu_item}
+            </a>
+          ))}
         </li>
         <li className="navbar__item slideInDown-2">
-          <a href="/" className="navbar__link">
-            Me contacter
-          </a>
+          {contentMenus.map(({ id, attributes }) => (
+            <a href={attributes.menu_link2} key={id} className="navbar__link">
+              {attributes.menu_item2}
+            </a>
+          ))}
         </li>
       </ul>
       <button className="navbar__burger" onClick={handleShowLinks}>
