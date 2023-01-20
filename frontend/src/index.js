f16ad6a48579a0853124765130f37c0ae30e782d
Gregory Juge <gregoryjuge5@gmail.com>
2023-01-20 10:31:16 +0100

    css adjust on mobile for text-bloc

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-// import "./index.css";
+import { hydrate, render } from "react-dom";
+// import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
